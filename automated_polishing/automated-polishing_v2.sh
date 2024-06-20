d5c02e3edf3aced0cb852d8983ae5e98bb406bd8
Patrick Grady <62141468+pgrady1322@users.noreply.github.com>
2024-06-20 19:02:34 -0400

    Update automated-polishing_v2.sh

--- automated_polishing/automated-polishing_v2.sh
+++ automated_polishing/automated-polishing_v2.sh
@@ -11,7 +11,7 @@
 
 ### Dependencies: meryl, minimap2, merfin, bcftools, raconL, winnowmap, falconc (pbipa)
 
-if [ "$#" -ne 6 ]; then
+if [ "$#" -ne 7 ]; then
     echo "Automated polishing of draft genomes."
     echo "Wrong number of arguments."
     echo ""
